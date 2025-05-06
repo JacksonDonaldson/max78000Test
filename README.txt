9cdd96b54e549020f92f90e35c3336ab0b65d22d
Jackson Donaldson <jackson88044@gmail.com>
2025-05-06 16:30:01 -0400

    gcr testing

--- README.txt
+++ README.txt
@@ -9,7 +9,7 @@ Build:
 Flashing to a real board:
     Requires installation of msdk tools. https://analogdevicesinc.github.io/msdk/USERGUIDE/
     cd program
-    make 
+    make release
     make flash.openocd
 
 Run test:
@@ -30,16 +30,23 @@ Run test:
 
 Sample output logs from QEMU and hardware are stored in output_QEMU and output_hardware respectively.
 
-Differences between QEMU and hardware output:
+Known differences between QEMU and hardware output:
+
+    Operations that may take some time in hardware occur instantly.
 
     ICC:
-        bit 7 of icc_ctrl is a 1 in hardware after disabling the cachem but not in qemu. This field is marked reserved in the user guide.
+        bit 7 of icc_ctrl is a 1 in hardware after disabling the cache but not in qemu. This field is marked reserved in the user guide.
 
     UART:
+        Hardware initializes UART to all zeros, while qemu initializes UART to the claimed Power-on-reset values.
+
         bits 0,1, 4, 5, 6, and 7 of uart_status differ between hardware and QEMU. They note the fullness of the rx and tx queue, so this is ok.
     
         bits 0 of PNR occasionally differs. It stores the status of UART CTS, which isn't modelled.
 
-        txpeek occasionally differs; QEMU has data in the queue, while hardware might not.
+        txpeek, fifo, and intfl occasionally differ when QEMU has data in the queue, while hardware might not, or the other way around
 
-        CTRL and frequency currently diverge significantly; this is because GCR, which resets UART, is not implemented. This will be fixed shortly.
+    GCR:
+        clkctrl bit 15 is a 0 in qemu but a 1 in hardware; this is a reserved field
+        clkctrl bits 25-29 occasionally differ; for simplicity, qemu reports all clocks as ready all the time
+        pclkdis1 bit 11 is a 1 in qemu but a 0 in hardware; this is a reserved field
--- program/src/main.c
+++ program/src/main.c
@@ -4,6 +4,7 @@
 #include "uart.h"
 #include "mxc_delay.h"
 #include "led.h"
+#include "trng.h"
 typedef unsigned int uint;
 
 #define reg(base, offset) (*(uint*) (base + offset))
@@ -33,6 +34,29 @@ void print_serial_state(char* base_addr){
     printf("\n");
 }
 
+void print_gcr_state(){
+    char* base_addr = (char*) 0x40000000;
+    printf("SYSCTRL: %08x\n", reg(base_addr, 0));
+    printf("RST0:    %08x\n", reg(base_addr, 4));
+    printf("CLKCTRL: %08x\n", reg(base_addr, 8));
+    printf("PM:      %08x\n", reg(base_addr, 0xc));
+    printf("PCLKDIV: %08x\n", reg(base_addr, 0x18));
+    printf("PCLKDIS0:%08x\n", reg(base_addr, 0x24));
+    printf("MEMCTRL: %08x\n", reg(base_addr, 0x28));
+    printf("MEMZ:    %08x\n", reg(base_addr, 0x2c));
+    printf("SYSST:   %08x\n", reg(base_addr, 0x40));
+    printf("RST1:    %08x\n", reg(base_addr, 0x44));
+    printf("PCLKDIS1:%08x\n", reg(base_addr, 0x48));
+    printf("EVENTEN: %08x\n", reg(base_addr, 0x4c));
+    printf("REVISIO: %08x\n", reg(base_addr, 0x50));
+    printf("SYSIE:   %08x\n", reg(base_addr, 0x54));
+    printf("ECCERR:  %08x\n", reg(base_addr, 0x64));
+    printf("ECCCED:  %08x\n", reg(base_addr, 0x68));
+    printf("ECCIE:   %08x\n", reg(base_addr, 0x6c));
+    printf("ECCADDR: %08x\n", reg(base_addr, 0x70));
+
+}
+
 void test_icc(){
     printf("Post-boot ICC0:\n");
     print_icc_state((char*)(0x4002a000));
@@ -49,7 +73,7 @@ void test_icc(){
 }
 
 void test_uart_num(int num, char* addr){
-    printf("Post-boot UART%d: \n", num);
+    printf("Starting UART%d: \n", num);
     print_serial_state(addr);
 
 
@@ -75,7 +99,7 @@ void test_uart_num(int num, char* addr){
 void test_uart(){
     char* uart0 = (char*)0x40042000;
     test_uart_num(0, uart0);
-    
+
     int first_char = MXC_UART_ReadCharacter(MXC_UART0);
     printf("read char %c\n", (char)first_char);
 
@@ -93,25 +117,100 @@ void test_uart(){
 
     printf("set UART0 freq: %08x\n", freq);
 
-    
-
-    //MXC_UART_Shutdown(MXC_UART0);
-    //printf("post-shutdown:\n");
     print_serial_state(uart0);
 
 }
 
-int main(){
-    test_uart();
-    test_icc();
+
+void test_gcr(){
+    printf("GCR state:\n");
+    print_gcr_state();
+}
+
+void test_mem_reset(uint* addr, uint reset){
+    volatile uint* memz = (uint*)0x4000002c;
+    *addr = 0x12345678;
+    printf("initial value: %08x\n", *addr);
+    *memz |= reset;
+    while(*memz & reset){}
+    printf("value after memz: %08x\n", *addr);
+}
+
+void test_trng(){
+    MXC_TRNG_Init();
+
+    for (int i = 0; i < 5; i++){
+        uint data;
+        MXC_TRNG_Random((unsigned char *)&data, 4);
+        printf("random data: %08x\n", data);
+    }
+    MXC_TRNG_Shutdown();
+}
+
+int main(){    
+    printf("started\n");
     
-
-    printf("main finished\n");
-
     while(1){
-        LED_Off(LED1);
-        MXC_Delay(500000);
-        LED_On(LED1);
-        MXC_Delay(500000);}
+        int incoming = MXC_UART_ReadCharacter(MXC_UART0);
+        switch (incoming)
+        {
+        case 'i':
+            printf("Test ICC:\n");
+            test_icc();
+            break;
+        case 'g':
+            printf("Test gcr:\n");
+            print_gcr_state();
+            break;
+        case 'r':
+            MXC_SYS_Reset_Periph(31);
+            break;
+        case 'u':
+            incoming = MXC_UART_ReadCharacter(MXC_UART0);
+            if(incoming == '1'){
+                MXC_SYS_Reset_Periph(12);
+                test_uart_num(1, (char*)0x40043000);
+            }
+            else if(incoming == '2'){
+                MXC_SYS_Reset_Periph(28);
+                test_uart_num(2, (char*)0x40044000);
+            }
+            else{
+                printf("Test uart\n");
+                test_uart();
+            }
+            break;
+        case 'c':
+            printf("Initial gcr state:\n");
+            print_gcr_state();
+
+            MXC_SYS_Clock_Select(MXC_SYS_CLOCK_IPO);
+
+            MXC_SYS_Clock_Select(MXC_SYS_CLOCK_ISO);
+
+            printf("Final gcr state:\n");
+            print_gcr_state();
+            break;
+        case 'z':
+            // Resetting the memory you're working in is a bad plan, hence the lack of 
+            // tests for RAM0 and RAM3.
+
+            //printf("ram0");
+            //test_mem_reset((uint*)0x20000010, MXC_F_GCR_MEMZ_RAM0);
+            printf("ram1");
+            test_mem_reset((uint*)0x20008010, MXC_F_GCR_MEMZ_RAM1);
+            printf("ram2");
+            test_mem_reset((uint*)0x20010010, MXC_F_GCR_MEMZ_RAM2);
+            //printf("ram3");
+            //test_mem_reset((uint*)0x2001c010, MXC_F_GCR_MEMZ_RAM3);
+            break;
+        case 't':
+            printf("Test TRNG\n");
+            test_trng();
+
+        default:
+            break;
+        }
+    }
         
 }
